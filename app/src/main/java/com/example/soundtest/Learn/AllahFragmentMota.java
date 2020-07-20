58218fa76207aef46f399d25306721754879e285
SUMON <engi.masihurrahman@gmail.com>
2020-07-20 07:43:20 +0600

    Allah Activity

--- app/src/main/java/com/example/soundtest/Learn/AllahFragmentMota.java
+++ app/src/main/java/com/example/soundtest/Learn/AllahFragmentMota.java
@@ -1,13 +1,19 @@
 package com.example.soundtest.Learn;
 
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageSwitcher;
+import android.widget.ImageView;
+import android.widget.ViewSwitcher;
 
 import com.example.soundtest.R;
 
@@ -16,7 +22,59 @@ import com.example.soundtest.R;
  */
 public class AllahFragmentMota extends Fragment {
 
+    private ImageSwitcher topSwitchermota,leftSwitchermota,rightSwitchermota,middleSwitchermota;
+    private ImageButton previousBtn,replayBtn,nextBtn;
+    MediaPlayer motaMedia;
+    private int currenthorkot = 0;
+    int position  = -1;
 
+    int[] motaTopImage = {
+            R.drawable.innalloha,
+
+            R.drawable.kolallohu,
+
+            R.drawable.samiallohu,
+
+            R.drawable.hududullohi,
+
+            R.drawable.uridullohu,
+
+            R.drawable.kholkullohi,
+    };
+
+    int[] motaRightImage = {
+            R.drawable.innalloha,
+            R.drawable.innalloha,
+            R.drawable.innalloha,
+
+
+            R.drawable.hududullohi,
+            R.drawable.baakharamodd,
+            R.drawable.baakharamodd,
+    };
+    int[] motaMidleImage = {
+            R.drawable.book11,
+            R.drawable.kolallohu,
+            R.drawable.kolallohu,
+
+            R.drawable.book11,
+            R.drawable.uridullohu,
+            R.drawable.uridullohu,
+    };
+    int[] motaLeftImage = {
+            R.drawable.book11,
+            R.drawable.book11,
+            R.drawable.samiallohu,
+
+            R.drawable.book11,
+            R.drawable.book11,
+            R.drawable.kholkullohi,
+    };
+
+
+    int [] motasound={R.raw.innalloha,R.raw.kolallohu,R.raw.samiallohu,R.raw.hidullohi,
+            R.raw.uridullohu,R.raw.kholqullahi,
+    };
     public AllahFragmentMota() {
         // Required empty public constructor
     }
@@ -26,7 +84,123 @@ public class AllahFragmentMota extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_allah_fragment_mota, container, false);
+        View view = inflater.inflate(R.layout.fragment_allah_fragment_mota, container, false);
+
+
+        previousBtn = view.findViewById(R.id.motaPreviousBtn);
+        replayBtn = view.findViewById(R.id.motaRepeatBtn);
+        nextBtn = view.findViewById(R.id.motaNextBtn);
+        topSwitchermota = view.findViewById(R.id.motaimageSwitherTop);
+        leftSwitchermota = view.findViewById(R.id.motaimageSwitherLeft);
+        middleSwitchermota = view.findViewById(R.id.motaimageSwitherMiddle);
+        rightSwitchermota = view.findViewById(R.id.motaimageSwitherRight);
+
+
+        motaMedia = MediaPlayer.create(getContext(),motasound[currenthorkot]);
+
+
+
+        topSwitchermota.setFactory(new ViewSwitcher.ViewFactory() {
+            @Override
+            public View makeView() {
+                ImageView imageView = new ImageView(getContext());
+                imageView.setLayoutParams(new ImageSwitcher.LayoutParams(ActionBar.LayoutParams.WRAP_CONTENT,ActionBar.LayoutParams.WRAP_CONTENT));
+                return imageView;
+            }
+        });
+
+        rightSwitchermota.setFactory(new ViewSwitcher.ViewFactory() {
+            @Override
+            public View makeView() {
+                ImageView imageView = new ImageView(getContext());
+                imageView.setLayoutParams(new ImageSwitcher.LayoutParams(ActionBar.LayoutParams.WRAP_CONTENT,ActionBar.LayoutParams.WRAP_CONTENT));
+                return imageView;
+            }
+        });
+        middleSwitchermota.setFactory(new ViewSwitcher.ViewFactory() {
+            @Override
+            public View makeView() {
+                ImageView imageView = new ImageView(getContext());
+                imageView.setLayoutParams(new ImageSwitcher.LayoutParams(ActionBar.LayoutParams.WRAP_CONTENT,ActionBar.LayoutParams.WRAP_CONTENT));
+                return imageView;
+            }
+        });
+        leftSwitchermota.setFactory(new ViewSwitcher.ViewFactory() {
+            @Override
+            public View makeView() {
+                ImageView imageView = new ImageView(getContext());
+                imageView.setLayoutParams(new ImageSwitcher.LayoutParams(ActionBar.LayoutParams.WRAP_CONTENT,ActionBar.LayoutParams.WRAP_CONTENT));
+                return imageView;
+            }
+        });
+
+        nextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(position<motaTopImage.length-1){
+
+                    position = position+1;
+                    topSwitchermota.setBackgroundResource(motaTopImage[position]);
+                    rightSwitchermota.setBackgroundResource(motaRightImage[position]);
+                    middleSwitchermota.setBackgroundResource(motaMidleImage[position]);
+                    leftSwitchermota.setBackgroundResource(motaLeftImage[position]);
+
+                    try {
+                        motaMedia = MediaPlayer.create(getContext(),motasound[currenthorkot]);
+                        currenthorkot++;
+                        motaMedia.start();
+                    }catch (Exception e){
+
+                    }
+
+                }
+
+            }
+
+        });
+
+        previousBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(position>0){
+
+                    position = position-1;
+                    topSwitchermota.setBackgroundResource(motaTopImage[position]);
+                    rightSwitchermota.setBackgroundResource(motaRightImage[position]);
+                    middleSwitchermota.setBackgroundResource(motaMidleImage[position]);
+                    leftSwitchermota.setBackgroundResource(motaLeftImage[position]);
+
+                    try {
+
+                        currenthorkot--;
+                        MediaPlayer.create(getContext(),motasound[currenthorkot]);
+                        motaMedia.start();
+                    }catch (Exception e){
+
+                    }
+
+
+                }
+
+            }
+        });
+
+        replayBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (motaMedia == null){
+                    motaMedia = MediaPlayer.create(getContext(),motasound[currenthorkot]);
+
+                }
+                motaMedia.start();
+            }
+        });
+
+
+        return view;
     }
 
 }
--- app/src/main/java/com/example/soundtest/Learn/PurFragment.java
+++ app/src/main/java/com/example/soundtest/Learn/PurFragment.java
@@ -1,13 +1,19 @@
 package com.example.soundtest.Learn;
 
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageSwitcher;
+import android.widget.ImageView;
+import android.widget.ViewSwitcher;
 
 import com.example.soundtest.R;
 
@@ -15,6 +21,43 @@ import com.example.soundtest.R;
  * A simple {@link Fragment} subclass.
  */
 public class PurFragment extends Fragment {
+    private ImageSwitcher topSwitcher_roPur,leftSwitcher_roPur,rightSwitcher_roPur,middleSwitcher_roPur;
+    private ImageButton previousBtn,replayBtn,nextBtn;
+    MediaPlayer roPurMedia;
+    private int currenthorkot = 0;
+    int position  = -1;
+
+    int[] roPurTopImage = {
+            R.drawable.amara,
+            R.drawable.rusulun,
+            R.drawable.garkan,
+            R.drawable.quranan,
+    };
+
+    int[] roPurRightImage = {
+            R.drawable.amara,
+            R.drawable.amara,
+            R.drawable.amara,
+            R.drawable.quranan,
+
+    };
+    int[] roPurMidleImage = {
+            R.drawable.book11,
+            R.drawable.rusulun,
+            R.drawable.rusulun,
+            R.drawable.quranan,
+    };
+    int[] roPurLeftImage = {
+            R.drawable.book11,
+            R.drawable.book11,
+            R.drawable.garkan,
+            R.drawable.quranan,
+    };
+
+
+    int [] roPursound={R.raw.amaro,R.raw.rusulun,R.raw.gorkon,R.raw.quraanun,
+
+    };
 
 
     public PurFragment() {
@@ -26,7 +69,131 @@ public class PurFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pur, container, false);
+        View view =  inflater.inflate(R.layout.fragment_pur, container, false);
+
+
+
+        previousBtn = view.findViewById(R.id.roMotaPreviousBtn);
+        replayBtn = view.findViewById(R.id.roMotaRepeatBtn);
+        nextBtn = view.findViewById(R.id.roMotaNextBtn);
+        topSwitcher_roPur = view.findViewById(R.id.roMotaimageSwitherTop);
+        leftSwitcher_roPur = view.findViewById(R.id.roMotaimageSwitherLeft);
+        middleSwitcher_roPur = view.findViewById(R.id.roMotaimageSwitherMiddle);
+        rightSwitcher_roPur = view.findViewById(R.id.roMotaimageSwitherRight);
+
+
+        roPurMedia = MediaPlayer.create(getContext(),roPursound[currenthorkot]);
+
+
+
+        topSwitcher_roPur.setFactory(new ViewSwitcher.ViewFactory() {
+            @Override
+            public View makeView() {
+                ImageView imageView = new ImageView(getContext());
+                imageView.setLayoutParams(new ImageSwitcher.LayoutParams(ActionBar.LayoutParams.WRAP_CONTENT,ActionBar.LayoutParams.WRAP_CONTENT));
+                return imageView;
+            }
+        });
+
+        rightSwitcher_roPur.setFactory(new ViewSwitcher.ViewFactory() {
+            @Override
+            public View makeView() {
+                ImageView imageView = new ImageView(getContext());
+                imageView.setLayoutParams(new ImageSwitcher.LayoutParams(ActionBar.LayoutParams.WRAP_CONTENT,ActionBar.LayoutParams.WRAP_CONTENT));
+                return imageView;
+            }
+        });
+        middleSwitcher_roPur.setFactory(new ViewSwitcher.ViewFactory() {
+            @Override
+            public View makeView() {
+                ImageView imageView = new ImageView(getContext());
+                imageView.setLayoutParams(new ImageSwitcher.LayoutParams(ActionBar.LayoutParams.WRAP_CONTENT,ActionBar.LayoutParams.WRAP_CONTENT));
+                return imageView;
+            }
+        });
+        leftSwitcher_roPur.setFactory(new ViewSwitcher.ViewFactory() {
+            @Override
+            public View makeView() {
+                ImageView imageView = new ImageView(getContext());
+                imageView.setLayoutParams(new ImageSwitcher.LayoutParams(ActionBar.LayoutParams.WRAP_CONTENT,ActionBar.LayoutParams.WRAP_CONTENT));
+                return imageView;
+            }
+        });
+
+        nextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(position<roPurTopImage.length-1){
+
+                    position = position+1;
+                    topSwitcher_roPur.setBackgroundResource(roPurTopImage[position]);
+                    rightSwitcher_roPur.setBackgroundResource(roPurRightImage[position]);
+                    middleSwitcher_roPur.setBackgroundResource(roPurMidleImage[position]);
+                    leftSwitcher_roPur.setBackgroundResource(roPurLeftImage[position]);
+
+                    try {
+                        roPurMedia = MediaPlayer.create(getContext(),roPursound[currenthorkot]);
+                        currenthorkot++;
+                        roPurMedia.start();
+                    }catch (Exception e){
+
+                    }
+
+                }
+
+            }
+
+        });
+
+        previousBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(position>0){
+
+                    position = position-1;
+                    topSwitcher_roPur.setBackgroundResource(roPurTopImage[position]);
+                    rightSwitcher_roPur.setBackgroundResource(roPurRightImage[position]);
+                    middleSwitcher_roPur.setBackgroundResource(roPurMidleImage[position]);
+                    leftSwitcher_roPur.setBackgroundResource(roPurLeftImage[position]);
+
+                    try {
+
+                        currenthorkot--;
+                        MediaPlayer.create(getContext(),roPursound[currenthorkot]);
+                        roPurMedia.start();
+                    }catch (Exception e){
+
+                    }
+
+
+                }
+
+            }
+        });
+
+        replayBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (roPurMedia == null){
+                    roPurMedia = MediaPlayer.create(getContext(),roPursound[currenthorkot]);
+
+                }
+                roPurMedia.start();
+            }
+        });
+
+
+
+
+
+
+
+
+        return view;
+
     }
 
 }
